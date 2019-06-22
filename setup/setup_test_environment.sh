23dcc3a9424c43621fb48120b9fed8399c462986
Peter Crosby <peter@headwall.io>
2019-06-21 21:34:33 -0600

    WIP: testing circleci

--- setup/setup_test_environment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "------------------------------------"
-echo "Running $BASH_SOURCE..."
-echo "------------------------------------"
-echo "Installing pipenv..."
-pip3 install pipenv
-echo "------------------------------------"
